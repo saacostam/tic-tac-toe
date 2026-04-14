242a08a4051bfdb76fc3183f28dd127b512d6bae
saacostam <73088662+saacostam@users.noreply.github.com>
2026-04-14 15:30:51 -0500

    feat(icons): add information circle icon

--- src/shared/icons/components/index.ts
+++ src/shared/icons/components/index.ts
@@ -3,6 +3,7 @@ export * from "./cube-transparent.icon";
 export * from "./exclamation-circle.icon";
 export * from "./fire.icon";
 export * from "./gamepad.icon";
+export * from "./information-circle.icon";
 export * from "./moon.icon";
 export * from "./sun.icon";
 export * from "./x-icon";
