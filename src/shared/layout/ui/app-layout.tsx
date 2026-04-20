e6133a8e5cf644ffd3e2cb30aaf9b8077bf9ffdc
saacostam <73088662+saacostam@users.noreply.github.com>
2026-04-19 19:43:11 -0500

    feat(app-layout): remove side-navbar

--- src/shared/layout/ui/app-layout.tsx
+++ src/shared/layout/ui/app-layout.tsx
@@ -8,33 +8,17 @@ import {
 	UnstyledButton,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { type PropsWithChildren, useCallback, useMemo } from "react";
+import { type PropsWithChildren, useCallback } from "react";
 import { ThemeToggle } from "@/features/theme/ui";
 import { useAdapters } from "@/shared/adapters/core/app";
 import { RouteName } from "@/shared/adapters/navigation/domain";
 import { Link } from "@/shared/adapters/navigation/ui";
 import { Logo } from "@/shared/components";
 
-const MAIN_LINKS: {
-	name: RouteName;
-	label: string;
-}[] = [] as const;
-
 export function AppLayout({ children }: PropsWithChildren) {
 	const { sessionAdapter, navigationAdapter } = useAdapters();
 
 	const [opened, { toggle }] = useDisclosure();
-
-	const links = useMemo(
-		() =>
-			MAIN_LINKS.map(({ name, label }) => ({
-				href: navigationAdapter.generateRoute({
-					name,
-				}),
-				label,
-			})),
-		[navigationAdapter.generateRoute],
-	);
 
 	const onClickRename = useCallback(
 		() => sessionAdapter.removeToken(),
@@ -42,15 +26,7 @@ export function AppLayout({ children }: PropsWithChildren) {
 	);
 
 	return (
-		<AppShell
-			header={{ height: 60 }}
-			navbar={{
-				width: 300,
-				breakpoint: "sm",
-				collapsed: { desktop: true, mobile: !opened },
-			}}
-			padding="md"
-		>
+		<AppShell header={{ height: 60 }} padding="md">
 			<AppShell.Header>
 				<Group h="100%" px="md">
 					<Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
@@ -64,27 +40,12 @@ export function AppLayout({ children }: PropsWithChildren) {
 							<Logo />
 						</UnstyledButton>
 						<Flex gap="lg">
-							<Group gap="lg" visibleFrom="sm">
-								{links.map(({ href, label }) => (
-									<UnstyledButton component={Link} key={href} to={href}>
-										{label}
-									</UnstyledButton>
-								))}
-							</Group>
 							<Button onClick={onClickRename}>Rename</Button>
 							<ThemeToggle />
 						</Flex>
 					</Group>
 				</Group>
 			</AppShell.Header>
-
-			<AppShell.Navbar py="md" px="lg">
-				{links.map(({ href, label }) => (
-					<UnstyledButton component={Link} key={href} to={href}>
-						{label}
-					</UnstyledButton>
-				))}
-			</AppShell.Navbar>
 
 			<AppShell.Main>
 				<Container mx="auto">{children}</Container>
